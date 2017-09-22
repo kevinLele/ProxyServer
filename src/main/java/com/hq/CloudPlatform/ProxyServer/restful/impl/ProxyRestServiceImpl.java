50658607dd04aea5208ec02c24d4d0f095f1fc6a
kevinLele <kevin.zhan@foxmail.com>
2017-09-22 11:30:46 +0800

    init

--- src/main/java/com/hq/CloudPlatform/ProxyServer/restful/impl/ProxyRestServiceImpl.java
+++ src/main/java/com/hq/CloudPlatform/ProxyServer/restful/impl/ProxyRestServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hq.CloudPlatform.ProxyServer.restful.impl;
 
+import com.hq.CloudPlatform.ProxyServer.entity.ProxyInfo;
 import com.hq.CloudPlatform.ProxyServer.exception.ServiceException;
 import com.hq.CloudPlatform.ProxyServer.restful.IProxyRestService;
 import com.hq.CloudPlatform.ProxyServer.service.IProxyInfoService;
@@ -19,6 +20,12 @@ public class ProxyRestServiceImpl implements IProxyRestService {
 
     @Override
     public String register(String url, String type) throws ServiceException {
+        ProxyInfo entity = new ProxyInfo();
+        entity.setOriginalUrl(url);
+        entity.setType(type);
+        entity.setProxyUrl("proxyUrl");
+
+        proxyInfoService.save(entity);
         return null;
     }
 }
