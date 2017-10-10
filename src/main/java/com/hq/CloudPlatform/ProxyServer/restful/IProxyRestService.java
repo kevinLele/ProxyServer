0ef692ab1bcb5046ba4dc193247473d631e0daac
kevinLele <kevin.zhan@foxmail.com>
2017-10-10 14:27:46 +0800

    init

--- src/main/java/com/hq/CloudPlatform/ProxyServer/restful/IProxyRestService.java
+++ src/main/java/com/hq/CloudPlatform/ProxyServer/restful/IProxyRestService.java
@@ -3,10 +3,8 @@ package com.hq.CloudPlatform.ProxyServer.restful;
 import com.hq.CloudPlatform.ProxyServer.exception.ServiceException;
 import com.hq.CloudPlatform.ProxyServer.sys.Constants;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by Administrator on 7/4/2017.
@@ -18,4 +16,9 @@ public interface IProxyRestService {
     @Produces(Constants.MediaType.APPLICATION_JSON)
     String register(@QueryParam("url") String url, @QueryParam("type") String type) throws ServiceException;
 
+    @POST
+    @Path("register")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(Constants.MediaType.APPLICATION_JSON)
+    String register(String jsonStr) throws ServiceException;
 }
--- src/main/java/com/hq/CloudPlatform/ProxyServer/restful/impl/ProxyRestServiceImpl.java
+++ src/main/java/com/hq/CloudPlatform/ProxyServer/restful/impl/ProxyRestServiceImpl.java
@@ -1,11 +1,13 @@
 package com.hq.CloudPlatform.ProxyServer.restful.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.hq.CloudPlatform.ProxyServer.entity.ProxyInfo;
 import com.hq.CloudPlatform.ProxyServer.exception.ServiceException;
 import com.hq.CloudPlatform.ProxyServer.restful.IProxyRestService;
 import com.hq.CloudPlatform.ProxyServer.service.IProxyInfoService;
 import com.hq.CloudPlatform.ProxyServer.utils.ConfigHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,6 +17,7 @@ import javax.ws.rs.Path;
 
 @Path("proxy")
 @Component
+@Slf4j
 public class ProxyRestServiceImpl implements IProxyRestService {
 
     @Autowired
@@ -32,22 +35,45 @@ public class ProxyRestServiceImpl implements IProxyRestService {
     @Override
     public String register(String url, String type) throws ServiceException {
         String key = DigestUtils.md5Hex(url);
-        StringBuffer proxyUrlSB = new StringBuffer();
-        proxyUrlSB.append(protocol).append("://")
-                .append(proxyIp).append(":")
-                .append(proxyPort).append("/")
-                .append(prefix).append("/")
-                .append(type).append("/")
-                .append(key);
+        ProxyInfo entity = findByKey(key);
 
-        ProxyInfo entity = new ProxyInfo();
-        entity.setOriginalUrl(url);
-        entity.setType(type);
-        entity.setProxyUrl(proxyUrlSB.toString());
-        entity.setUrlKey(key);
-        long id = proxyInfoService.save(entity);
-        entity.setId(id);
+        if (null == entity) {
+            StringBuffer proxyUrlSB = new StringBuffer();
+            proxyUrlSB.append(protocol).append("://")
+                    .append(proxyIp).append(":")
+                    .append(proxyPort).append("/")
+                    .append(prefix).append("/")
+                    .append(type).append("/")
+                    .append(key);
+
+            entity = new ProxyInfo();
+            entity.setOriginalUrl(url);
+            entity.setType(type);
+            entity.setProxyUrl(proxyUrlSB.toString());
+            entity.setUrlKey(key);
+            long id = proxyInfoService.save(entity);
+            entity.setId(id);
+        }
 
         return JSON.toJSONString(entity);
+    }
+
+    @Override
+    public String register(String jsonStr) throws ServiceException {
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+        String url = jsonObject.getString("url");
+        String type = jsonObject.getString("type");
+
+        return register(url, type);
+    }
+
+    private ProxyInfo findByKey(String key) {
+        try {
+            return proxyInfoService.findByKey(key);
+        } catch (ServiceException e) {
+            log.error("Finding by Key error!", e);
+        }
+
+        return null;
     }
 }
